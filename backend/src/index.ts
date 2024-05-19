274d6e6e6b7bad507e4bf719ab83fc28e4d2a08a
madhavsahi <madhavsahi16@gmail.com>
2024-05-19 23:03:28 +0530

    added rate limiting to protect sensitive routes in backend script

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
-	limit: 3, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: 4, // Limit each IP to 4 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Redis, Memcached, etc. See below.
