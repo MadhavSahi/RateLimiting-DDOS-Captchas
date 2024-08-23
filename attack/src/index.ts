8f3a518cb81d2cc25227d9c1c6ee23a4c15fd312
madhavsahi <madhavsahi16@gmail.com>
2024-08-23 22:48:38 +0530

    added rate limiting to protect sensitive routes in backend script

--- attack/src/index.ts
+++ attack/src/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function sendRequest(otp: string) {
   let data = JSON.stringify({
-    email: "madhav16@gmail.com",
+    email: "madhavsahi16@gmail.com",
     otp: otp,
     newPassword: "NewPassword:-" + otp,
   });
