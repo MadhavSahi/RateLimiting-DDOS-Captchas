2207ad4d9137a187b10dabab281229f203dee43c
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-08-30 23:44:50 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
 	windowMs: 5 * 60 * 1000, // 5 minutes
-	limit: 3, // Limit each IP to 3 requests per `window` (here, per 15 minutes).
+	limit: 5, // Limit each IP to 3 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Redis, Memcached, etc. See below.
