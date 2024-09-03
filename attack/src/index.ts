78911436e3df704c85b6f8fc33119a658b3ef99b
Madhav Sahi <63411260+MadhavSahi@users.noreply.github.com>
2024-09-03 23:58:10 +0530

    Update index.ts

--- attack/src/index.ts
+++ attack/src/index.ts
@@ -4,7 +4,7 @@ async function sendRequest(otp: string) {
   let data = JSON.stringify({
     email: "madhavsahi16@gmail.com",
     otp: otp,
-    newPassword: "NewPassword:-" + otp,
+    newPassword: "NewPassword :-" + otp,
   });
 
   let config = {
